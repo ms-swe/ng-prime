2c1c62157372e0896570294881f9fe8f850fbb43
MichaS <micha@s.com>
2024-06-03 13:54:37 +0200

    fix primeflex and routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,14 +1,14 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-  { path: '', redirectTo: 'playground', pathMatch: 'full' },
+  { path: '', redirectTo: 'playground2', pathMatch: 'full' },
   {
     path: 'playground',
     loadChildren: () =>
       import('./playground/playground.routes').then((m) => m.PLAYGROUND_ROUTES),
   },
   {
-    path: 'playground',
+    path: 'playground2',
     loadChildren: () =>
       import('./playground2/playground2.routes').then(
         (m) => m.PLAYGROUND2_ROUTES,
