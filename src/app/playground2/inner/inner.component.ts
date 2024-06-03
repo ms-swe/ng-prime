207398db3a8b8ed73b1bda64b3b7a5665a550637
MichaS <micha@s.com>
2024-06-03 13:38:13 +0200

    Simple menu, layout, unused import

--- src/app/playground2/inner/inner.component.ts
+++ src/app/playground2/inner/inner.component.ts
@@ -1,4 +1,4 @@
-import { Component, input, model, ModelSignal } from '@angular/core';
+import { Component, input, model } from '@angular/core';
 import { CardModule } from 'primeng/card';
 import { RatingModule } from 'primeng/rating';
 import { FormsModule } from '@angular/forms';
